8453fa0a867043be8befbc6ca1971d91f29d236d
Samseppiol <j_hage@live.com>
2019-05-09 10:41:32 +1000

    Fix incorrect action parameter

--- ext/cfndsl/statement.rb
+++ ext/cfndsl/statement.rb
@@ -4,7 +4,7 @@ def create_statement(config, type, default)
       statement['Effect'] = config[type].has_key?('Effect') ? config[type]['Effect'] : default[type]['Effect']
       statement['Principal'] = config[type].has_key?('Principal') ? config[type]['Principal'] : default[type]['Principal']
       statement['Resource'] = config[type].has_key?('Resource') ? config[type]['Resource'] : default[type]['Resource']
-      statement['Actions'] = config[type].has_key?('Actions') ? config[type]['Actions'] : default[type]['Actions']
+      statement['Action'] = config[type].has_key?('Actions') ? config[type]['Actions'] : default[type]['Actions']
       statement['Condition'] = config[type]['Conditions'] if config[type].has_key?('Conditions')
     else
       statement = default[type]
